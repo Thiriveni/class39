6b189e9a4baccf27a4bd69adbd0c3f9b8ea4b84c
Thiriveni <67469883+Thiriveni@users.noreply.github.com>
2020-12-18 18:57:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var  database;
+
+var gameState=0;
+var form,player,game;
+
+var playerCount;
+
+var allPlayers;
+var car1,car2,car3,car4;
+var cars;
+var car1Img,car2Img,car3Img,car4Img;
+var trackImg;
+
+function preload(){
+  car1Img=loadImage("images/car1.png");
+  car2Img=loadImage("images/car2.png");
+  car3Img=loadImage("images/car3.png");
+  car4Img=loadImage("images/car4.png");
+
+  trackImg=loadImage("images/track.jpg");
+}
+
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  database = firebase.database();
+  game=new Game();
+  game.getState();
+  game.start();
+  
+  
+
+  
+}
+
+function draw(){
+  if(playerCount===4){
+      game.updateState(1);
+  }
+
+  if(gameState===1){
+    game.play();
+    
+  }
+ 
+}
+
